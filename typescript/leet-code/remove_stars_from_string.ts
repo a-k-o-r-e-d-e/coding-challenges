e37a229c579be6526891df483b51582720579b8d
Korede <adeyanju.akorede1@gmail.com>
2023-11-16 11:45:53 +0100

    leetcode:TS: remove stars from string - stack approach - 2 pointer approach

---
+++ typescript/leet-code/remove_stars_from_string.ts
@@ -0,0 +1,78 @@
+/**
+ * You are given a string s, which contains stars *.
+ * In one operation, you can:
+ * Choose a star in s.
+ * Remove the closest non-star character to its left, as well as remove the star itself.
+ * Return the string after all stars have been removed.
+ *
+ * Note:
+ * The input will be generated such that the operation is always possible.
+ * It can be shown that the resulting string will always be unique.
+ *
+ * Example 1:
+ * Input: s = "leet**cod*e"
+ * Output: "lecoe"
+ * Explanation: Performing the removals from left to right:
+ * - The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+ * - The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+ * - The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+ * There are no more stars, so we return "lecoe".
+ *
+ * Example 2:
+ * Input: s = "erase*****"
+ * Output: ""
+ * Explanation: The entire string is removed, so we return an empty string.
+ *
+ * Constraints:
+ * 1 <= s.length <= 105
+ * s consists of lowercase English letters and stars *.
+ * The operation above can be performed on s.
+ */
+
+function removeStarsStackSolution(s: string): string {
+  // We use a stack and push each none star element of the string into the stack
+  // when we encounter a star, we simply pop the top of the star as that is assured to be the closest none star element
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    //   const v = s[i];
+    if (s[i] === "*") {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+
+  return stack.join("");
+}
+
+function removeStars2PointerSolution(s: string): string {
+    // Approach
+    // Use two pointers technique. 
+    // One keeps track of position in original string, 
+    // and the other keeps track of position in the resulting string.
+    // As we scan through the original string, 
+    // if we encounter a star, we move the resulting string pointer one step back. effectively deleting the leftmost non star character
+    // If it is a regular character, we copy it to the resulting string.
+  let chars = Array.from(s);
+  let writer = 0;
+
+  for (let seeker = 0; seeker < s.length; seeker++) {
+    //   const v = s[i];
+    if (chars[seeker] === "*") {
+      writer--;
+    } else {
+      chars[writer] = chars[seeker];
+      writer++
+    }
+  }
+
+  return chars.slice(0, writer).join("");
+}
+
+
+console.log("Result ::", removeStars2PointerSolution("leet**cod*e"));
+console.log("Result ::", removeStars2PointerSolution("erase*****"));
+console.log("Result ::", removeStars2PointerSolution("*****"));
+console.log("Result ::", removeStars2PointerSolution("erase"));
+console.log("Result ::", removeStars2PointerSolution(""));

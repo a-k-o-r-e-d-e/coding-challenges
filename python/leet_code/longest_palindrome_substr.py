f9ba498956acfea55fdd60236e7cb058f11b0e69
Korede <adeyanju.akorede1@gmail.com>
2025-11-10 07:36:07 +0100

    [lC] [PY]: longest palindrome substr (expand from center solution)

--- python/leet_code/longest_palindrome_substr.py
+++ python/leet_code/longest_palindrome_substr.py
@@ -39,3 +39,28 @@ class BruteForceSolution:
             return False
 
         return self.is_palindrome(s, left+1, right-1)
+    
+
+class ExpandFromCenterSolution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        
+        max_str = s[0]
+
+        for i in range(len(s) -1):
+            odd = self.expand_from_center(s, i, i)
+            even = self.expand_from_center(s, i, i+1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
+    
+    def expand_from_center(self, s:str, left: int, right: int)-> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right +=1
+        return s[left +1: right]
